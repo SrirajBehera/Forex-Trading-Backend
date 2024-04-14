26ff704f703fe6b096932eb1746f15bbac49de4b
Sriraj Behera <behera.sriraj2911@gmail.com>
2024-04-14 21:38:55 +0530

    updated appModule for static serve in api/public for swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,18 @@ import { appConfig } from './config/app.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
 import { FxRatesModule } from './modules/fx-rates/fx-rates.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, appConfig],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src', 'modules', 'api', 'public'),
+      serveRoot: '/api',
     }),
     MongooseModule.forRoot(databaseConfig().uri),
     AuthModule,
